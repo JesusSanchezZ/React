0e4f67290191317dad4fcdc5da32c4dd5e361453
JesusSanchezZ <jsan.zambrano@gmail.com>
2022-07-25 19:49:01 -0500

    Sección 17 Material ui

---
+++ 08-journal-app/src/journal/pages/JournalPage.jsx
@@ -0,0 +1,30 @@
+import { IconButton } from '@mui/material';
+import { AddOutlined } from '@mui/icons-material';
+import { JournalLayout } from '../layout/JournalLayout';
+import { NoteView, NothingSelectedView } from '../views';
+
+export const JournalPage = () => {
+    return (
+        <JournalLayout>
+            {/* <Typography>Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit minus exercitationem voluptate obcaecati! Eveniet veritatis, vel velit recusandae repellat, atque repudiandae repellendus facilis aliquid voluptatibus quisquam nostrum dolor odio. Mollitia.</Typography> */}
+
+            <NothingSelectedView />
+
+            <IconButton
+                size='large'
+                sx={{
+                    color: 'white',
+                    backgroundColor: 'error.main',
+                    ':hover': { backgroundColor: 'error.main', opacity: 0.9},
+                    position: 'fixed',
+                    right: 50,
+                    bottom: 50
+                }}
+            >
+                <AddOutlined sx={{ fontSize: 30 }}/>
+            </IconButton>
+
+            {/* <NoteView /> */}
+        </JournalLayout>
+    )
+}
--- 08-journal-app/src/main.jsx
+++ 08-journal-app/src/main.jsx
@@ -1,10 +1,37 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import { JournalApp } from './JournalApp';
+import './styles.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <JournalApp />
+    </BrowserRouter>
   </React.StrictMode>
 )
+
+
+
+/**
+ * Preparacíon del proyecto:
+ * creación del proyecto: npm create vite
+ *  cd "nombre del proyecto"
+ *  npm install
+ *  ejecución del proyecto: npm run dev  // modo desarrollo
+ * 
+ * Instalación de React Router
+ *  npm install react-router-dom@6       // instala la versión 6 de React router
+ * 
+ * Instalación de material ui
+ *  npm install @mui/material @emotion/react @emotion/styled
+ * Agregar al index.html la fuente Roboto
+ *  <link
+ *    rel="stylesheet"
+ *    href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+ *  />
+ *  
+ * Instalación de iconos
+ *  npm install @mui/icons-material
+ */
